3b0d4d5db1a55a9fc7d719dc4b5fb3eac3a577e2
TasminaAktermonika <tasminaaktermonika@gmail.com>
2018-10-08 14:51:06 +0600

    first commit

---
+++ sumaiya akter.c
@@ -0,0 +1,10 @@
+#include<studio.h>
+int main(){
+      float a,b,c,s area
+      printf("please enter 3 value"=);
+      scanf("&f &f &f"&a &b &c);
+      s=(a+b+c)/2;
+      area=sqrt(s*(s-a)*(s-b)*(s-c));
+      printf("the area is=if/n,"area);
+
+}
